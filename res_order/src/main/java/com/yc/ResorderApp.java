9a5e631936dbbc7f448b8eb0d9252c8ae877df01
xinyewu <257160660@qq.com>
2023-11-27 21:28:04 +0800

    文件提交

--- res_order/src/main/java/com/yc/ResorderApp.java
+++ res_order/src/main/java/com/yc/ResorderApp.java
@@ -1,11 +1,9 @@
 package com.yc;
 
 
-import com.yc.config.MyBalanceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
